e5afa3867b1625a7ab0dd7e12c45f2e743372db7
Duncan McGreggor <oubiwann@gmail.com>
2016-01-20 18:38:18 -0600

    Started on OTP namespace.

---
+++ src/clojang/jinterface/otp.clj
@@ -0,0 +1,120 @@
+(ns clojang.jinterface.otp
+  (:require [clojang.util :as util])
+  (:import [com.ericsson.otp.erlang
+            AbstractNode
+            OtpLocalNode
+            OtpNode
+            OtpMbox
+            OtpPeer
+            OtpSelf]))
+
+;;; >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+;;; Macros and helper functions
+;;; >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+(defn- make-otp-name [name-symbol]
+  (util/make-jinterface-name name-symbol "Otp"))
+
+(defmacro defotp [otp-symbol docstring args]
+  "Create an Erlang OTP object constructor."
+  `(defn ~otp-symbol ~docstring [~@args]
+     (new ~(make-otp-name otp-symbol) ~@args)))
+
+(defmacro defotp-protected [otp-symbol docstring]
+  "Return an ``Exception`` that warns that a constructor is not provided for
+  this object."
+  `(defn ~otp-symbol ~docstring []
+     (Exception (str "The ~(make-otp-name otp-symbol) class is not"
+                      "instantiable by the developer."))))
+
+;;; >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+;;; OTP constructors
+;;; >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+(defotp-protected local-node
+  "This class represents local node types. It is used to group the node types
+  ``OtpNode`` and ``OtpSelf``.")
+
+(defotp node
+  "Represents a local OTP node. This class is used when you do not wish to
+  manage connections yourself - outgoing connections are established as
+  needed, and incoming connections accepted automatically. This class
+  supports the use of a mailbox API for communication, while management of
+  the underlying communication mechanism is automatic and hidden from the
+  application programmer.
+
+  Once an instance of this class has been created, obtain one or more
+  mailboxes in order to send or receive messages. The first message sent to
+  a given node will cause a connection to be set up to that node. Any
+  messages received will be delivered to the appropriate mailboxes.
+
+  To shut down the node, call ``(close)``. This will prevent the node from
+  accepting additional connections and it will cause all existing
+  connections to be closed. Any unread messages in existing mailboxes can
+  still be read, however no new messages will be delivered to the mailboxes.
+
+  Note that the use of this class requires that EPMD (Erlang Port Mapper
+  Daemon) is running on each cooperating host. This class does not start EPMD
+  automatically as Erlang does, you must start it manually or through some
+  other means. See the Erlang documentation for more information about this."
+  [arg])
+
+;;; >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+;;; OTP protocols
+;;; >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+(defprotocol AbstractNodeObject
+  (get-alivename [this] "Get the alivename part of the hostname.")
+  (get-cookie [this] "Get the authorization cookie used by this node.")
+  (create-transport [this addr port-num] "Create instance of ``OtpTransport``.")
+  (create-server-transport [this port-num] "Create instance of ``OtpServerTransport``.")
+  (get-hostname [this] "Get the hostname part of the nodename.")
+  (get-name [this] "Get the name of this node.")
+  (set-cookie [this value] "Set the authorization cookie used by this node.")
+  (->str [this] "Return a string representation of the node."))
+
+(extend-type AbstractNode AbstractNodeObject
+  (get-alivename [this]
+    (.alive this))
+  (get-cookie [this]
+    (.cookie this))
+  (create-transport [this addr port-num]
+    (.createTransport this addr port-num))
+  (create-server-transport [this port-num]
+    (.createServerTrasport this port-num))
+  (get-hostname [this]
+    (.host this))
+  (get-name [this]
+    (.node this))
+  (set-cookie [this value]
+    (.setCookie this value))
+  (->str [this]
+    (.toString this)))
+
+(defprotocol LocalNodeObject
+  (create-pid [this] "Create an Erlang pid.")
+  (create-port [this] "Create an Erlang port.")
+  (create-ref [this] "Create an Erlang ref.")
+  (get-port [this] "Get the port number used by this node.")
+  ;; protected
+  ; (get-epmd [this] "Get the EPMD socket.")
+  ; (set-epmd [this socket] "Set the Epmd socket after publishing this nodes
+  ;                          listen port to EPMD.")
+  )
+
+(extend-type OtpLocalNode LocalNodeObject
+  (create-pid [this]
+    (.createPid this))
+  (create-port [this]
+    (.createPort this))
+  (create-ref [this]
+    (.createRef this))
+  (get-port [this]
+    (.port this))
+  ;; protected
+  ; (get-epmd [this]
+  ;   (.getEpmd this))
+  ; (set-epmd [this socket]
+  ;   (.setEpmd this socket))
+  )
+
