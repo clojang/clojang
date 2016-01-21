9eaedc370d2c61be9d9d62d9a408ff904ff8bc15
Duncan McGreggor <oubiwann@gmail.com>
2016-01-20 21:55:15 -0600

    Updated docs build.

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@ JINTERFACE := $(shell ls -1 $(ERL_LIBS)/lib/|grep jinterface)
 JINTERFACE_VER := $(strip $(subst \", , $(word 2, $(subst -, , $(JINTERFACE)))))
 JINTERFACE_JAR := jinterface-$(JINTERFACE_VER).jar
 JINTERFACE_BUILD := /tmp/jinterface/$(JINTERFACE_VER)
+JINTERFACE_FILES := $(ERL_LIBS)/lib/jinterface-$(JINTERFACE_VER)/java_src/com/ericsson/otp/erlang/*.java
 CLOJURE_DEP := $(strip $(shell grep "org.clojure/clojure" project.clj))
 CLOJURE_VER := $(subst ], , $(word 3, $(CLOJURE_DEP)))
 JAR := $(PROJ)-$(VERSION).jar
--- resources/make/docs.mk
+++ resources/make/docs.mk
@@ -5,7 +5,12 @@ DOCS_DIR = $(ROOT_DIR)/docs
 REPO = $(shell git config --get remote.origin.url)
 DOCS_BUILD_DIR = $(DOCS_DIR)/build
 DOCS_PROD_DIR = $(DOCS_DIR)/master
+CURRENT = $(DOCS_PROD_DIR)/current
+ERL_DOCS_SRC = ~/Dropbox/Docs/Erlang
+ERL_DOCS_DIR = $(CURRENT)/erlang
+JAVA_DOCS_DIR = $(ERL_DOCS_DIR)/java
 DOCS_GIT_HACK = $(DOCS_DIR)/.git
+REMOTE_DOCS_HOST = http://oubiwann.github.io/clojang/current/java
 LOCAL_DOCS_HOST = localhost
 LOCAL_DOCS_PORT = 5099
 
@@ -14,18 +19,50 @@ $(DOCS_GIT_HACK):
 
 devdocs: local-docs
 	@echo "\nRunning docs server on http://$(LOCAL_DOCS_HOST):$(LOCAL_DOCS_PORT)..."
-	@lein simpleton $(LOCAL_DOCS_PORT) file :from $(DOCS_PROD_DIR)/current
+	@lein simpleton $(LOCAL_DOCS_PORT) file :from $(CURRENT)
 
 docs: local-docs
 
-local-docs: clean-docs
-	@echo "\nBuilding docs ...\n"
-	@lein codox
+local-docs: clean-docs pre-docs clojang-docs java-docs erl-docs
 
 prod-docs: clean-docs $(DOCS_GIT_HACK) local-docs
 
+pre-docs:
+	@echo "\nBuilding docs ...\n"
+
+java-docs:
+	@javadoc -public \
+	         -use \
+	         -version \
+	         -author \
+	         -nodeprecated \
+	         -keywords \
+	         -quiet \
+	         -d $(JAVA_DOCS_DIR) $(JINTERFACE_FILES)
+
+erl-docs:
+	@mkdir -p $(ERL_DOCS_DIR) $(DOCS_PROD_DIR)/doc
+	@cp \
+	$(ERL_DOCS_SRC)/lib/jinterface-$(JINTERFACE_VER)/doc/html/users_guide.html \
+	$(ERL_DOCS_SRC)/lib/jinterface-$(JINTERFACE_VER)/doc/html/jinterface_users_guide.html \
+	$(ERL_DOCS_DIR)
+	@cp -r \
+	$(ERL_DOCS_SRC)/doc/js \
+	$(ERL_DOCS_SRC)/doc/otp_doc.css \
+	$(ERL_DOCS_SRC)/doc/erlang-logo.png \
+	$(CURRENT)
+	@patch \
+	$(CURRENT)/erlang/users_guide.html \
+	resources/erlang_users_guide.patch
+	@patch \
+	$(CURRENT)/erlang/jinterface_users_guide.html \
+	resources/jinterface_users_guide.patch
+
+clojang-docs:
+	@lein codox
+
 clean-docs:
-	@rm -rf $(DOCS_PROD_DIR)/current
+	@rm -rf $(CURRENT)
 
 setup-temp-repo: $(DOCS_GIT_HACK)
 	@echo "\nSetting up temporary git repos for gh-pages ...\n"
