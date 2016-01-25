cf206be5061dbcb4b6f8a21870ef7e7f2afb4983
Duncan McGreggor <oubiwann@gmail.com>
2016-01-25 00:41:08 -0600

    Started working on the mid-level code.
    These protocol methods will convert Clojure EDN <-> Erlang JInterface terms
    without the need of having to manually create individual Erlang data types for
    each message sent or received.

---
+++ src/clojang/core.clj
@@ -0,0 +1,65 @@
+(ns clojang.core
+  (:require [clojure.string :as clj-string]
+            [dire.core :refer [with-handler!]]
+            [clojang.jinterface.erlang.atom :as atom]
+            [clojang.jinterface.erlang.boolean :as boolean]
+            [clojang.jinterface.erlang.string :as string]
+            [clojang.jinterface.erlang.types :as types]
+            [clojang.util :as util])
+  (:import [com.ericsson.otp.erlang])
+  (:refer-clojure :exclude [atom boolean]))
+
+(defprotocol EDNConverter
+  "Convert EDN."
+  (edn->term [this] "Convert Clojure EDN to JInterface Erlang types."))
+
+(extend-protocol EDNConverter
+  ;; nil
+  nil
+  (edn->term [edn]
+    (types/atom "undefined"))
+  ;; atom / keyword
+  clojure.lang.Keyword
+  (edn->term [edn]
+    (types/atom (name edn)))
+  ;; boolean
+  java.lang.Boolean
+  (edn->term [edn]
+    (types/boolean edn))
+  ;; tuple /vector
+  ;; list
+  ;; string
+  java.lang.String
+  (edn->term [edn]
+    (types/string edn)))
+
+(defprotocol TermConverter
+  "Convert JInterface Erlang terms."
+  (term->edn [this] "Convert JInterface Erlang types to EDN."))
+
+(extend-protocol TermConverter
+  ;; atom / keyword & undefined
+  com.ericsson.otp.erlang.OtpErlangAtom
+  (term->edn [erl-obj]
+    (let [trans (atom/->str erl-obj)]
+      (if (= trans "undefined")
+        nil
+        (keyword trans))))
+  ;; boolean
+  com.ericsson.otp.erlang.OtpErlangBoolean
+  (term->edn [erl-obj]
+    (boolean/get-value erl-obj))
+  ;; tuple /vector
+  ;; list
+  ;; string
+  com.ericsson.otp.erlang.OtpErlangString
+  (term->edn [erl-obj]
+    (clj-string/replace
+      (string/->str erl-obj)
+      #"\""
+      "")))
+
+;;; Aliases
+
+(def ->erlang #'edn->term)
+(def ->clojure #'term->edn)
---
+++ test/clojang/test/core_test.clj
@@ -0,0 +1,54 @@
+(ns clojang.test.core-test
+  (:require [clojure.test :refer :all]
+            [dire.core :refer [with-handler!]]
+            [clojang.core :as clojang]
+            [clojang.jinterface.erlang.atom :as atom]
+            [clojang.jinterface.erlang.boolean :as boolean]
+            [clojang.jinterface.erlang.string :as string]
+            [clojang.jinterface.erlang.types :as types]
+            [clojang.util :as util])
+  (:import [com.ericsson.otp.erlang])
+  (:refer-clojure :exclude [atom boolean]))
+
+(deftest ^:unit edn->term-test
+  ;; nil
+  (let [term (clojang/edn->term nil)]
+    (is (= com.ericsson.otp.erlang.OtpErlangAtom (type term)))
+    (is (= "undefined" (atom/->str term))))
+  ;; atom / keyword
+  (let [term (clojang/edn->term :a)]
+    (is (= com.ericsson.otp.erlang.OtpErlangAtom (type term)))
+    (is (= "a" (atom/->str term))))
+  ;; boolean
+  (let [term (clojang/edn->term true)]
+    (is (= com.ericsson.otp.erlang.OtpErlangBoolean (type term)))
+    (is (= "true" (boolean/->str term))))
+  ;; tuple /vector
+  ;; list
+  ;; string
+  (let [term (clojang/edn->term "a")]
+    (is (= com.ericsson.otp.erlang.OtpErlangString (type term)))
+    (is (= "\"a\"" (string/->str term)))))
+
+(deftest ^:unit term->edn-test
+  ;; nil
+  (let [edn (clojang/term->edn (types/atom "undefined"))]
+    (is (= nil (type edn)))
+    (is (= nil edn)))
+  ;; atom / keyword
+  (let [edn (clojang/term->edn (types/atom "a"))]
+    (is (= clojure.lang.Keyword (type edn)))
+    (is (= :a edn)))
+  ;; boolean
+  (let [edn (clojang/term->edn (types/boolean true))]
+    (is (= java.lang.Boolean (type edn)))
+    (is (= true edn)))
+  ;; tuple /vector
+  ;; list
+  ;; string
+  (let [edn (clojang/term->edn (types/string "a"))]
+    (is (= java.lang.String (type edn)))
+    (is (= "a" edn))))
+
+
+
