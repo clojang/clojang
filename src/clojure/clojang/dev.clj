fede803d2845b9615a5f8a021fa5b95960a980e8
Duncan McGreggor <oubiwann@gmail.com>
2016-01-30 20:32:04 -0600

    Added connection and fixed require for nodes.

--- src/clojure/clojang/dev.clj
+++ src/clojure/clojang/dev.clj
@@ -3,8 +3,9 @@
   prototyping, &c."}
   clojang.dev
   (:require [clojang.jinterface.erlang.types :as ji-types]
+            [clojang.jinterface.otp.connection :as connection]
             [clojang.jinterface.otp.messaging :as messaging]
-            [clojang.jinterface.otp.node :as node]
+            [clojang.jinterface.otp.nodes :as nodes]
             [clojang.core :as clojang]
             [clojang.mbox :as mbox]
             [clojang.node :as node]
