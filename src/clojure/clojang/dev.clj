33297907728c02ace91efbae408d9899c9174aae
Duncan McGreggor <oubiwann@gmail.com>
2016-01-30 18:24:26 -0600

    Added more namespaces in the requires.

--- src/clojure/clojang/dev.clj
+++ src/clojure/clojang/dev.clj
@@ -2,8 +2,11 @@
   "A development namespace that imports other useful namespaces for easy
   prototyping, &c."}
   clojang.dev
-  (:require [clojang.jinterface.otp.messaging :as messaging]
+  (:require [clojang.jinterface.erlang.types :as ji-types]
+            [clojang.jinterface.otp.messaging :as messaging]
+            [clojang.jinterface.otp.node :as node]
             [clojang.core :as clojang]
             [clojang.mbox :as mbox]
             [clojang.node :as node]
+            [clojang.types :as types]
             [clojang.util :as util]))
