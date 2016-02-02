8132f906338d6ef4de18bb126e2acd3df68e7e34
Duncan McGreggor <oubiwann@gmail.com>
2016-02-02 00:06:42 -0600

    Added docstrings.

--- src/clojure/clojang/rpc.clj
+++ src/clojure/clojang/rpc.clj
@@ -1,4 +1,12 @@
-(ns clojang.rpc
+(ns ^{:doc
+  "The ``clojang.rpc`` namespace has no analong in the JInterface package. This
+  namepsace intends to provide a similar interface as that provided by the
+  ``clojang.conn`` namespace: a set of functions for sending and receiving
+  messages to and from remote processes. In this case, however, there is a
+  very specfic client we expect to be receiving requests from and replying to:
+  an RPC client, and one that expects this namespace to provide functions
+  compatible with the RPC capabilities of an Erlang/OTP ``gen_server``."}
+  clojang.rpc
   (:require [potemkin :refer [import-vars]]
             [clojang.core :as clojang]
             [clojang.conn :as conn]
@@ -36,7 +44,7 @@
 
 (defn msg->rpc-data
   "Convert RPC message data to a Clojure map, if the message contains RPC
-  data."
+  data. If not, set the ``:rpc-data`` value to ``nil``."
   [msg-data]
   (let [is-rpc (rpc? msg-data)]
     {:rpc? is-rpc
@@ -66,14 +74,42 @@
   (connection/send connx (util/->str-arg dest) (clojang/->erlang msg)))
 
 (defn receive
-  ""
+  "This function is called by the RPC server in anticipation of a message from
+  an Erlang RPC client. As such, it parses the ``OtpMsg`` for RPC content,
+  identifies the function to call on behalf of the client, makes this call
+  (using ``eval``), and then sends the result to the calling ``OtpPid`` (being
+  that of the client).
+
+  Note that in order for an Erlang RPC client to accept a message from an RPC
+  server, the message sent by the server has to conform to a very specific
+  format, a format that is not speficied anywhere except the OTP ``gen_server``
+  code. As such, this Clojure function sends a reply to the client using this
+  format. Since the format is only specificed in OTP source code, it may change
+  in the future, in which case this function will need to be updated.
+
+  The request sent by the RPC client boils down to the following chain of calls
+  (some have been elided):
+   * ``rpc:do_call/3``
+   * ``gen_server:call/3``
+   * ``gen:do_call/4``
+
+  The response that the RPC client is expected is created/formated in the
+  following functions, and these are what govern how this particular Clojure
+  function sends its results back to the RPC client, especially the ``reply/2``
+  function:
+   * ``gen_server:handle_msg/*``
+   * ``gen_server:reply/2``
+
+  In particular, this sending code is what we used:
+  ``To ! {Tag, Reply}`` (which, in LFE, would be ``(! pid `#(,tag ,reply))``)."
   [& args]
   (let [connx (first args)
-        self (node/get-pid (conn/get-self connx))
         msg-data (apply receive-msg args)
         client-pid (get-in msg-data [:rpc-data :calling-pid])
-        client-node (get-in msg-data [:rpc-data :calling-node])
         client-ref (get-in msg-data [:rpc-data :calling-ref])
+        ;; XXX the next assignment actually needs to pass the args
+        ;; to a function that will eval mod/func/args ... the following
+        ;; is just a placeholder
         result (get-in msg-data [:rpc-data :args])
         response (clojang/->erlang [client-ref result])
         ]
