8061b7368a52c6c59e8bbe483d16d7738c2e58aa
Duncan McGreggor <oubiwann@gmail.com>
2017-01-15 03:30:42 -0600

    Added linting targets.

--- dev-resources/make/test.mk
+++ dev-resources/make/test.mk
@@ -9,3 +9,29 @@ check-integration:
 
 check-all:
 	@lein with-profile +testing test :all
+
+kibit:
+	@lein with-profile +testing kibit
+
+bikeshed:
+	@lein with-profile +testing bikeshed
+
+base-eastwood:
+	@lein with-profile +testing eastwood "$(EW_OPTS)"
+
+yagni:
+	@lein with-profile +testing yagni
+
+eastwood:
+	@EW_OPTS="{:namespaces [:source-paths]}" make base-eastwood
+
+lint: kibit eastwood bikeshed yagni
+
+lint-unused:
+	@EW_OPTS="{:linters [:unused-fn-args :unused-locals :unused-namespaces :unused-private-vars :wrong-ns-form] :namespaces [:source-paths]}" make base-eastwood
+
+lint-ns:
+	@EW_OPTS="{:linters [:unused-namespaces :wrong-ns-form] :namespaces [:source-paths]}" make base-eastwood
+
+ancient:
+	@lein with-profile +testing ancient check.check-profiles
