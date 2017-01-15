fd5aeeb1e45ff2a2514843d4995f98821bf4d707
Duncan McGreggor <oubiwann@gmail.com>
2017-01-15 14:30:37 -0600

    Renamed vars, functions, and protocols.

--- src/clojure/clojang/core.clj
+++ src/clojure/clojang/core.clj
@@ -20,13 +20,13 @@
   [process-name msg]
   )
 
-(declare edn->term term->edn)
+(declare clj->term term->clj)
 
 (defn ->erl-array [xs]
-  (into-array (types/object) (map edn->term xs)))
+  (into-array (types/object) (map clj->term xs)))
 
 (defn ->clj-vector [xs]
-  (map #(term->edn %) (into [] xs)))
+  (map #(term->clj %) (into [] xs)))
 
 (defn vector->ji-tuple
   "Convert a Clojure vector into an Erlang JInterface tuple."
@@ -63,201 +63,199 @@
                                     erl-map-keys))]
     (apply hash-map (interleave map-keys map-vals))))
 
-(defprotocol EDNConverter
-  "Convert EDN."
-  (edn->term [this]
-    "Convert Clojure EDN to JInterface Erlang types."))
+(defprotocol ClojureTypesConverter
+  (clj->term [this]
+    "Convert Clojure types to Erlang/JInterface types."))
 
-(extend-protocol EDNConverter
+(extend-protocol ClojureTypesConverter
   ;; nil
   nil
-  (edn->term [edn]
+  (clj->term [clj-obj]
     (types/atom "undefined"))
   ;; atom / keyword
   clojure.lang.Keyword
-  (edn->term [edn]
-    (types/atom (name edn)))
+  (clj->term [clj-obj]
+    (types/atom (name clj-obj)))
   ;; symbol
   clojure.lang.Symbol
-  (edn->term [edn]
-    (types/atom (str edn)))
+  (clj->term [clj-obj]
+    (types/atom (str clj-obj)))
   ;; boolean
   java.lang.Boolean
-  (edn->term [edn]
-    (types/boolean edn))
+  (clj->term [clj-obj]
+    (types/boolean clj-obj))
   ;; tuple /vector
   clojure.lang.PersistentVector
-  (edn->term [edn]
-    (vector->ji-tuple edn))
+  (clj->term [clj-obj]
+    (vector->ji-tuple clj-obj))
   clojure.lang.PersistentVector$ChunkedSeq
-  (edn->term [edn]
-    (vector->ji-tuple edn))
+  (clj->term [clj-obj]
+    (vector->ji-tuple clj-obj))
   ;; list
   clojure.lang.PersistentList
-  (edn->term [edn]
-    (list->ji-list edn))
+  (clj->term [clj-obj]
+    (list->ji-list clj-obj))
   clojure.lang.PersistentList$EmptyList
-  (edn->term [edn]
+  (clj->term [clj-obj]
     (list->ji-list))
   ;; char
   java.lang.Character
-  (edn->term [edn]
-    (types/char edn))
+  (clj->term [clj-obj]
+    (types/char clj-obj))
   ;; map
   clojure.lang.PersistentArrayMap
-  (edn->term [edn]
-    (map->ji-map edn))
+  (clj->term [clj-obj]
+    (map->ji-map clj-obj))
   ;; long
   java.lang.Long
-  (edn->term [edn]
-    (types/long edn))
+  (clj->term [clj-obj]
+    (types/long clj-obj))
   clojure.lang.BigInt
-  (edn->term [edn]
-    (types/long edn))
+  (clj->term [clj-obj]
+    (types/long clj-obj))
   java.math.BigInteger
-  (edn->term [edn]
-    (types/long edn))
+  (clj->term [clj-obj]
+    (types/long clj-obj))
   ;; byte
   java.lang.Byte
-  (edn->term [edn]
-    (types/byte edn))
+  (clj->term [clj-obj]
+    (types/byte clj-obj))
   ;; char
   java.lang.Character
-  (edn->term [edn]
-    (types/char edn))
+  (clj->term [clj-obj]
+    (types/char clj-obj))
   ;; int
   java.lang.Integer
-  (edn->term [edn]
-    (types/int edn))
+  (clj->term [clj-obj]
+    (types/int clj-obj))
   ;; short
   java.lang.Short
-  (edn->term [edn]
-    (types/short edn))
+  (clj->term [clj-obj]
+    (types/short clj-obj))
   ; ;; uint
   ; java.lang.Integer
-  ; (edn->term [edn]
-  ;   (types/uint edn))
+  ; (clj->term [clj-obj]
+  ;   (types/uint clj-obj))
   ; ;; ushort
   ; java.lang.Short
-  ; (edn->term [edn]
-  ;   (types/ushort edn))
+  ; (clj->term [clj-obj]
+  ;   (types/ushort clj-obj))
   ;; float
   java.lang.Float
-  (edn->term [edn]
-    (types/float edn))
+  (clj->term [clj-obj]
+    (types/float clj-obj))
   ;; double
   java.lang.Double
-  (edn->term [edn]
-    (types/double edn))
+  (clj->term [clj-obj]
+    (types/double clj-obj))
   ;; string
   java.lang.String
-  (edn->term [edn]
-    (types/string edn))
+  (clj->term [clj-obj]
+    (types/string clj-obj))
   ;; XXX JInterface objects
   com.ericsson.otp.erlang.OtpConnection
-  (edn->term [obj]
+  (clj->term [obj]
     obj)
   com.ericsson.otp.erlang.OtpNode
-  (edn->term [obj]
+  (clj->term [obj]
     obj)
   com.ericsson.otp.erlang.OtpMbox
-  (edn->term [obj]
+  (clj->term [obj]
     obj)
   com.ericsson.otp.erlang.OtpErlangPid
-  (edn->term [obj]
+  (clj->term [obj]
     obj)
   com.ericsson.otp.erlang.OtpErlangObject
-  (edn->term [obj]
+  (clj->term [obj]
     obj))
 
-(defprotocol TermConverter
-  "Convert JInterface Erlang terms."
-  (term->edn [this]
-    "Convert JInterface Erlang types to EDN."))
+(defprotocol ErlangTermConverter
+  (term->clj [this]
+    "Convert Erlang/JInterface terms to Clojure types."))
 
-(extend-protocol TermConverter
+(extend-protocol ErlangTermConverter
   ;; nil
   nil
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     erl-obj)
   ;; atom / keyword & undefined
   com.ericsson.otp.erlang.OtpErlangAtom
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     (let [trans (atom/->str erl-obj)]
       (if (= trans "undefined")
         nil
         (keyword (clj-string/replace trans "'" "")))))
   ;; boolean
   com.ericsson.otp.erlang.OtpErlangBoolean
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     (boolean/get-boolean-value erl-obj))
   ;; tuple /vector
   com.ericsson.otp.erlang.OtpErlangTuple
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     (into [] (->clj-vector (tuple/get-elements erl-obj))))
   ;; list
   com.ericsson.otp.erlang.OtpErlangList
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     (->clj-vector erl-obj))
   ;; char
   com.ericsson.otp.erlang.OtpErlangChar
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     (int/get-char-value erl-obj))
   ;; map
   com.ericsson.otp.erlang.OtpErlangMap
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     (ji-map->map erl-obj))
   ;; long
   com.ericsson.otp.erlang.OtpErlangLong
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     (int/get-bigint-value erl-obj))
   ;; byte
   com.ericsson.otp.erlang.OtpErlangShort
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     (int/get-byte-value erl-obj))
   ;; char
   com.ericsson.otp.erlang.OtpErlangShort
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     (int/get-char-value erl-obj))
   ;; int
   com.ericsson.otp.erlang.OtpErlangShort
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     (int/get-int-value erl-obj))
   ;; short
   com.ericsson.otp.erlang.OtpErlangShort
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     (int/get-short-value erl-obj))
   ;; uint
   com.ericsson.otp.erlang.OtpErlangShort
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     (int/get-uint-value erl-obj))
   ;; ushort
   com.ericsson.otp.erlang.OtpErlangShort
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     (int/get-ushort-value erl-obj))
   ;; float
   com.ericsson.otp.erlang.OtpErlangFloat
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     (float/get-float-value erl-obj))
   ;; double
   com.ericsson.otp.erlang.OtpErlangDouble
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     (float/get-double-value erl-obj))
   ;; pid
   com.ericsson.otp.erlang.OtpErlangPid
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     erl-obj)
   ;; ref
   com.ericsson.otp.erlang.OtpErlangRef
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     erl-obj)
   ;; XXX Clojure/Java objects ...
   java.lang.Object
-  (term->edn [obj]
+  (term->clj [obj]
     obj)
   ;; string
   com.ericsson.otp.erlang.OtpErlangString
-  (term->edn [erl-obj]
+  (term->clj [erl-obj]
     (clj-string/replace
       (string/->str erl-obj)
       #"\""
@@ -265,5 +263,5 @@
 
 ;;; Aliases
 
-(def ->erlang "An alias for ``edn->term``" #'edn->term)
-(def ->clojure "An alias for ``term->edn``" #'term->edn)
+(def ->erlang "An alias for ``clj->term``" #'clj->term)
+(def ->clojure "An alias for ``term->clj``" #'term->clj)
