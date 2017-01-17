d94aded3a03ea589716f8d80df6e2fdf1e9bca32
Duncan McGreggor <oubiwann@gmail.com>
2017-01-16 22:49:07 -0600

    Moved epmd function stub into own ns.

--- src/clojure/clojang/util.clj
+++ src/clojure/clojang/util.clj
@@ -4,14 +4,6 @@
   (:import [clojure.lang Reflector]
            [com.ericsson.otp.erlang]))
 
-;; XXX support the following keys:
-;; [-d|-debug] [DbgExtra...] [-port No] [-daemon] [-relaxed_command_check]
-(defn start-epmd
-  "Start the Erlang Port Mapper Daemon external (OS) process needed by
-  JInterface for creating nodes and communicating with other nodes."
-  []
-  'TBD)
-
 (defn ->str-arg [arg]
   (condp #(%1 %2) arg
     keyword? (name arg)
