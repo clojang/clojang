70fc51c81808af3f56a429b8b20fa33853e2c310
Duncan McGreggor <oubiwann@gmail.com>
2018-07-18 23:24:49 -0500

    Bumped version for release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojang "0.5.0-SNAPSHOT"
+(defproject clojang "0.5.0"
   :description "Erlang/OTP Communications in Clojure (wraps jiface + JInterface)"
   :url "https://github.com/clojang/clojang"
   :scm {
