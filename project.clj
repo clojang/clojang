4f7f283a951a6edd2b9c61e1f76591ffaaa765f3
Duncan McGreggor <oubiwann@gmail.com>
2018-07-19 00:07:42 -0500

    Bumped version for 0.6.0 release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojang "0.6.0-SNAPSHOT"
+(defproject clojang "0.6.0"
   :description "Erlang/OTP Communications in Clojure (wraps jiface + JInterface)"
   :url "https://github.com/clojang/clojang"
   :scm {
@@ -12,8 +12,8 @@
     [org.clojure/tools.namespace]
     [org.clojure/tools.reader]]
   :dependencies [
-    [clojang/agent "0.5.0"]
-    [clojang/jiface "0.5.0"]
+    [clojang/agent "0.6.0"]
+    [clojang/jiface "0.6.0"]
     [clojusc/trifl "0.3.0"]
     [org.clojure/clojure "1.9.0"]
     [org.clojure/core.memoize "0.7.1"]
@@ -65,7 +65,7 @@
       :repl-options {:init-ns clojang.dev}
       :bootclasspath true
       :java-agents [
-        [clojang/agent "0.5.0"]]
+        [clojang/agent "0.6.0"]]
       :jvm-opts [
         ; "-verbose:class"
         ;"-Dheadless"
